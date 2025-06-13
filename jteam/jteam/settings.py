3fc642d5d502a92ff3fd51f4e29a17e6d45287d8
Липатов Пётр <pafos.light@gmail.com>
2025-06-14 01:13:08 +0400

    🚀 feat(core, games, account, auth): improve UX across games list, profile & login

--- jteam/jteam/settings.py
+++ jteam/jteam/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -110,7 +111,9 @@ DATE_INPUT_FORMATS = [
     '%d/%m/%Y',
 ]
 
-USE_I18N = False
+USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
